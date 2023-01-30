e3cea571e507f058da798c45d798484a28b1a424
Oleg <89175955650@mail.ru>
2023-01-30 21:47:39 +0300

    Next commit to GitHub.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    pass
+import openai
+import telebot
+
+openai.api_key = ""
+bot = telebot.TeleBot("5833252827:AAFSvcWFqc6-2Tovojdy7WIEhXbnZHd9dZs")
 
 
-if __name__ == "__main__":
-    main()
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+    )
+    bot.send_message(chat_id=message.from_user.id, text=response["choices"][0]["text"])
+
+
+bot.polling()
